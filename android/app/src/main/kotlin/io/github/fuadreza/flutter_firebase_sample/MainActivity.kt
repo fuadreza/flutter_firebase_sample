16d858e96bc30ca246581118e1ab38b7d4d2ef7c
fuadreza <fuadreza1011@gmail.com>
2020-09-04 23:41:59 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/io/github/fuadreza/flutter_firebase_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fuadreza.flutter_firebase_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_sample/src/login/login.dart';
+
+void main() {
+  runApp(Login());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: <Widget>[
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/multitasking.png",
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
